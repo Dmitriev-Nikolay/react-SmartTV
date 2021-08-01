db4a7b158ae3fb7defdd1c5cfeab1a338636acc7
Nikolay <dmitriev9229@mail.ru>
2021-08-01 18:36:36 +0300

    Auto stash before revert of "fix routing"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-
+import { Route, Switch } from 'react-router-dom';
 import { Home, Order } from './pages';
 
 const App = () => {
   return (
     <div className="wrapper">
-      <Route exact path="/" component={ Home } />
-      <Route exact path="/order" component={ Order } />
-      <Redirect from='/' to='/' />
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/order" component={ Order } />
+      </Switch>
     </div>
   );
 };
