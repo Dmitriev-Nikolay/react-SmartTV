642cccc233bd7602e72d7be5b5040639809dafeb
Nikolay <dmitriev9229@mail.ru>
2021-08-01 18:41:40 +0300

    gh-pages fix routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
 import { Home, Order } from './pages';
 
 const App = () => {
   return (
     <div className="wrapper">
-      <Switch>
         <Route exact path="/" component={ Home } />
         <Route exact path="/order" component={ Order } />
-      </Switch>
     </div>
   );
 };
